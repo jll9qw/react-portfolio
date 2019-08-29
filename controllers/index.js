6bd415ddff0bc04133e0e0cab5908b4184d57b32
jll9qw <lloyd804@gmail.com>
2019-08-28 23:08:45 -0400

    initializing git repo

---
+++ controllers/index.js
@@ -0,0 +1,41 @@
+const db = require("../models");
+
+// Defining methods for the WorksController
+module.exports = {
+  findAll: function(req, res) {
+    db.Work.find(req.query)
+      // .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Work.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  searchByTerm: (req, res) => {
+    db.Work.find(
+      { $text: { $search: req.params.query } },
+      { score: { $meta: "textScore" } }
+    )
+      .sort({ score: { $meta: "textScore" } })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Work.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Work.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Work.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
---
+++ routes/api/index.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const controller = require("../../controllers");
+
+// Matches with "/api/herbs/"
+router
+  .route("/")
+  .get(controller.findAll)
+  .post(controller.create);
+
+// Matches with "/api/herbs/:id"
+router
+  .route("/:id")
+  .get(controller.findById)
+  .put(controller.update)
+  .delete(controller.remove);
+
+// Matches with "/api/herbs/search/:term"
+router.route("/search/:query").get(controller.searchByTerm);
+
+module.exports = router;
